00459d30b3be6fa644efea1521fac14d8377ebee
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-09-23 19:20:48 -0400

    adding more features

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ myButton.addEventListener('click', () => {
     const userInput = myInput.value;
     myName.textContent = userInput;
 });
+
+const myPinkBackgroundButton = document.getElementById("pink-background");
+const myBlueBackgroundButton = document.getElementById("blue-background");
+const myGrayBackgroundButton = document.getElementById("gray-background");
+const randomizeButton = document.getElementById("randomize");
+
+const myBody = document.getElementById('body');
+
+myPinkBackgroundButton.addEventListener('click', () => {
+    myBody.style.background = "pink";
+})
+
+myBlueBackgroundButton.addEventListener('click', () => {
+    myBody.style.background = "blue";
+})
+
+myGrayBackgroundButton.addEventListener('click', () => {
+    myBody.style.background = "rgb(134, 134, 134)";
+})
+
+randomize.addEventListener('click', () => {
+    if (myBody.style.background === "pink") {
+        myBody.style.background = "blue";
+    } else if (myBody.style.background === "blue") {
+        myBody.style.background = "rgb(134, 134, 134)";
+    } else if (myBody.style.background === "rgb(134, 134, 134)") {
+        myBody.style.background = "pink";
+    }
+})
